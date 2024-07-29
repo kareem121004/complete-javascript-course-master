50072510efa370ce58c6b0b00ae884fc74e1dee0
kareem121004 <karimadel11112004@gmail.com>
2024-07-29 19:13:41 +0300

    commit

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -11,7 +11,7 @@ if (passTest) hasDriverLicense = true;
 
 if (hasDriversLicense) console.log("I can drive :D");
 
-*/
+
 
 
 function logger() {
@@ -19,7 +19,7 @@ function logger() {
 }
 
 
-// Calling, Running, OR Invoking the Function. 
+// Calling, Running, OR Invoking the Function.
 logger();
 logger();
 
@@ -37,8 +37,6 @@ console.log(nameAge("Karim", 20));
 
 const hi = nameAge("Karim", 20);
 console.log(hi);
-
-
 
 
 
@@ -65,4 +63,100 @@ console.log(calcAge2(2004));
 
 
 
+// Arrow Function
 
+const calcAge3 = BirthYear => 2024 - BirthYear;
+
+const age3 = calcAge3(2004);
+console.log(age3);
+
+
+const yearsUntillRetirement = BirthYear => 60 - (2024 - BirthYear);
+
+const retirementAge = yearsUntillRetirement(2004);
+console.log(retirementAge);
+
+const yearsUntillRetirement2 = (BirthYear, firstName) => {
+    const yearsLeft = 60 - (2024 - BirthYear);
+
+    const fullSentence = `Hi ${firstName} You Have ${yearsLeft} Years Until Retirement...`;
+
+    return fullSentence;
+}
+
+console.log(yearsUntillRetirement2(2004, "Karim"));
+
+
+
+const fruitGenerator = (apples, oranges) => {
+    const juice = `juice with ${apples} apples and ${oranges} oranges...`;
+    return juice;
+}
+
+console.log(fruitGenerator(2, 3));
+
+
+
+
+// Function Calling other Functions...
+
+const fruitPieces = (fruit) => fruit * 4;
+
+const fruitGenerator = (apples, oranges) => {
+    const applePieces = fruitPieces(apples);
+    const orangePieces = fruitPieces(oranges);
+
+    const juice = `juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange...`;
+    return juice;
+}
+
+console.log(fruitGenerator(2, 3));
+
+
+const calcAge = BirthYear => 2024 - BirthYear;
+
+
+const yearsUntillRetirement = (BirthYear, firstName) => {
+
+    const age = calcAge(BirthYear);
+    const yearsLeft = 60 - age;
+
+    if (yearsLeft > 0) {
+        console.log(`Hi ${firstName} You Have ${yearsLeft} Years Until Retirement...`);
+        return yearsLeft;
+    }
+    else
+    return -1;
+}
+
+console.log(yearsUntillRetirement(2004, "adel"));
+console.log(yearsUntillRetirement(1950, "adel"));
+
+
+
+
+// First Coding Challenge!!
+
+// Write your code below. Good luck! 🙂 
+const calcAverage = (firstNum, secondNum, thirdNum) => (firstNum + secondNum + thirdNum) / 3;
+
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+
+const scoreDolphins2 = calcAverage(85, 54, 41);
+const scoreKoalas2 = calcAverage(23, 34, 27);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+    if (avgDolphins >= avgKoalas * 2)
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+
+    else if (avgKoalas >= avgDolphins * 2)
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    else
+        console.log("No team wins...");
+}
+
+checkWinner(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins2, scoreKoalas2);
+
+*/
