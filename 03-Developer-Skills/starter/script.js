2d6560d5422f40488e46209249265227fc5d64d3
kareem121004 <karimadel11112004@gmail.com>
2024-08-07 17:50:51 +0300

    commit

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const printForecast = function (array) {
+  let info = '';
+  for (let i = 0; i < array.length; i++) {
+    info += `${array[i]}C in ${i + 1} days... `;
+  }
+  console.log('...' + info);
+};
+
+printForecast([17, 21, 23]);
