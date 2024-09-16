ec9a69cee7db18cf0e08362bc766e4f642e766ee
kareem121004 <karimadel11112004@gmail.com>
2024-09-16 11:54:17 +0300

    commit

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,19 @@
-'use strict';
+"use strict";
+
+const bookings = [];
+const createBokking = function (flightNum, numOfPassengers = 1, price = 299) {
+  // ES5 OLD WAY
+  //   numOfPassengers = numOfPassengers || 1;
+  //   price = price || 299;
+  const booking = {
+    flightNum,
+    numOfPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBokking("ll444");
+createBokking("LA567", 2);
+createBokking("AB123", undefined, 477);
