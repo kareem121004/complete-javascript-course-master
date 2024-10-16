f3435be8fe54427f4672088a660009c874f73264
kareem121004 <karimadel11112004@gmail.com>
2024-10-16 08:29:33 +0300

    commit new

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -21,9 +21,6 @@ const closeModal = function () {
 
 btnsOpenModal.forEach(open => open.addEventListener('click', openModal));
 
-// for (let i = 0; i < btnsOpenModal.length; i++)
-//   btnsOpenModal[i].addEventListener('click', openModal);
-
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
@@ -32,3 +29,54 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+///////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////////////////
+
+///////////////////////////////////////
+// Selecting, Creating, and Deleting Elements
+
+// Selecting
+
+console.log(document.documentElement);
+console.log(document.body);
+console.log(document.head);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+console.log(document.getElementById('section--1'));
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+console.log(document.getElementsByClassName('btn'));
+console.log(document.querySelectorAll('.btn'));
+
+// Creating
+// .insertAdjacentHTMl
+
+// Method 1
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.insertAdjacentElement('afterbegin', message);
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true)); // copy message to display it more than once
+
+// header.before(message); // sibling of the header but before it
+// header.after(message); // sibling of the header but after it
+
+// Method 2
+// const html = `<div class="cookie-message"> karim adel mohamed </div>`;
+// header.insertAdjacentHTML('afterbegin', html);
+
+// Deleting Elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove(); // recent way
+    // message.parentElement.removeChild(message); // old way
+  });
