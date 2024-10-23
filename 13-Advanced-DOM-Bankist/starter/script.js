5985a8cb75f4e5a504e8230d8c9828996cc3d884
kareem121004 <karimadel11112004@gmail.com>
2024-10-23 08:26:48 +0300

    commit

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -124,6 +124,18 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
 ///////////////////////////////////////
+
+// Sticky Navigation
+
+const coord = section1.getBoundingClientRect();
+window.addEventListener('scroll', function () {
+  if (this.window.scrollY > coord.top) nav.classList.add('sticky');
+  else {
+    nav.classList.remove('sticky');
+  }
+});
+
+///////////////////////////////////////
 ///////////////////////////////////////
 /*
 ///////////////////////////////////////
