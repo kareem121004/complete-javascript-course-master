e0e2db02f43291658c7b5974e3b70f9058303921
kareem121004 <karimadel11112004@gmail.com>
2024-12-10 12:42:03 +0200

    commit

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -13,6 +13,8 @@ const inputElevation = document.querySelector(".form__input--elevation");
 
 // Using GeoLocation
 
+let map, MapEvent;
+
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -23,20 +25,48 @@ if (navigator.geolocation) {
 
       const coords = [latitude, longitude];
 
-      const map = L.map("map").setView(coords, 13);
+      map = L.map("map").setView(coords, 13);
 
       L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup("A pretty CSS popup.<br> Easily customizable.")
-        .openPopup();
+      map.on("click", function (MapE) {
+        MapEvent = MapE;
+        form.classList.remove("hidden");
+        inputDistance.focus();
+      });
     },
     function () {
       alert(`Can't get your location...`);
     }
   );
 }
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  inputDistance.value = inputDuration.value = inputCadence.value = "";
+  const { lat, lng } = MapEvent.latlng;
+  const newcoords = [lat, lng];
+  L.marker(newcoords)
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 25,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent(inputType.value)
+    .openPopup();
+  form.classList.add("hidden");
+});
+
+inputType.addEventListener("change", function () {
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+});
